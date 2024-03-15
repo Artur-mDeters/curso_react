aa0a276c363a75dc778caef3a1f52b27b0fab8f7
Artur Deters <arturdamotta@gmail.com>
2024-03-14 22:32:33 -0300

    css-and-hooks

---
+++ Component.jsx
@@ -0,0 +1,14 @@
+import "./Component.css";
+
+
+const Component = () => {
+  return (
+    <div>
+        <h1 className="h1">css no componente</h1>
+        <p className="p">Teste de css</p>
+        <button className="myButton">Meu botão</button>
+    </div>
+  )
+}
+
+export default Component
